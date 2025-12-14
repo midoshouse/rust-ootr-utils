22c438173704ba1bc74dec3823d0dbf505cffdd1
Fenhl <fenhl@fenhl.net>
2025-12-14 16:36:27 +0000

    Remove redundant branch param from deps

--- assets/pre-commit.ps1
+++ assets/pre-commit.ps1
@@ -7,18 +7,18 @@ function ThrowOnNativeFailure {
     }
 }
 
-cargo lcheck
+cargo check
 ThrowOnNativeFailure
 
-cargo lcheck --all-features
+cargo check --all-features
 ThrowOnNativeFailure
 
 # copy the tree to the WSL file system to improve compile times
-wsl rsync --delete -av /mnt/c/Users/fenhl/git/github.com/midoshouse/rust-ootr-utils/stage/ /home/fenhl/wslgit/github.com/midoshouse/rust-ootr-utils/ --exclude target
+wsl -d ubuntu-m2 rsync --mkpath --delete -av /mnt/c/Users/fenhl/git/github.com/midoshouse/rust-ootr-utils/stage/ /home/fenhl/wslgit/github.com/midoshouse/rust-ootr-utils/ --exclude target
 ThrowOnNativeFailure
 
-wsl env -C /home/fenhl/wslgit/github.com/midoshouse/rust-ootr-utils cargo lcheck
+wsl -d ubuntu-m2 env -C /home/fenhl/wslgit/github.com/midoshouse/rust-ootr-utils /home/fenhl/.cargo/bin/cargo check
 ThrowOnNativeFailure
 
-wsl env -C /home/fenhl/wslgit/github.com/midoshouse/rust-ootr-utils cargo lcheck --all-features
+wsl -d ubuntu-m2 env -C /home/fenhl/wslgit/github.com/midoshouse/rust-ootr-utils /home/fenhl/.cargo/bin/cargo check --all-features
 ThrowOnNativeFailure
